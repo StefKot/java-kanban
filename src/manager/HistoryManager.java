b4db6571d943f6f98e7b98700b38676707e7ade6
StefKot <96449266+StefKot@users.noreply.github.com>
2024-11-27 00:41:47 +0300

    HistoryManagers

---
+++ src/manager/HistoryManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import task.Task;
+
+import java.util.List;
+
+public interface HistoryManager {
+
+    List<Task> getHistory();
+
+    void addHistory(Task task);
+
+}
---
+++ src/manager/InMemoryHistoryManager.java
@@ -0,0 +1,27 @@
+package manager;
+
+import task.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    public static final int MAX_SIZE = 10;
+    private final List<Task> history = new ArrayList<>();
+
+    @Override
+    public List<Task> getHistory() {
+        return history;
+    }
+
+    @Override
+    public void addHistory(Task task) {
+        if (task == null) {
+            return;
+        }
+        if (history.size() == MAX_SIZE) {
+            history.removeFirst();
+        }
+        history.add(task);
+    }
+}
