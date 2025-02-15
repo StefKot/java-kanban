c357029aea62452837604a872b8e3640b7edaa98
StefKot <96449266+StefKot@users.noreply.github.com>
2025-02-15 13:39:00 +0300

    New functions for starting and stopping the server, constructor for HttpServer was corrected

--- src/api/server/HttpTaskServer.java
+++ src/api/server/HttpTaskServer.java
@@ -3,25 +3,38 @@ package api.server;
 import api.handlers.*;
 import com.sun.net.httpserver.HttpServer;
 import manager.Managers;
+import manager.TaskManager;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class HttpTaskServer {
     private static final int PORT = 8080;
+    private final HttpServer httpServer;
+
+    public HttpTaskServer(TaskManager taskManager) throws IOException {
+        httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
+
+        httpServer.createContext("/hello", new BaseHandler(taskManager));
+        httpServer.createContext("/tasks", new TasksHandler(taskManager));
+        httpServer.createContext("/epics", new EpicsHandler(taskManager));
+        httpServer.createContext("/subtasks", new SubTasksHandler(taskManager));
+        httpServer.createContext("/history", new HistoryHandler(taskManager));
+        httpServer.createContext("/priority", new PriorityTasksHandler(taskManager));
+    }
+
+    public void start() {
+        httpServer.start();
+        System.out.println("HTTP server is running on " + PORT + " port!");
+    }
+
+    public void stop() {
+        httpServer.stop(0);
+        System.out.println("HTTP server stopped.");
+    }
 
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
-
-        httpServer.createContext("/hello", new BaseHandler(Managers.getDefault()));
-        httpServer.createContext("/tasks", new TasksHandler(Managers.getDefault()));
-        httpServer.createContext("/epics", new EpicsHandler(Managers.getDefault()));
-        httpServer.createContext("/subtasks", new SubTasksHandler(Managers.getDefault()));
-        httpServer.createContext("/history", new HistoryHandler(Managers.getDefault()));
-        httpServer.createContext("/priority", new PriorityTasksHandler(Managers.getDefault()));
-
-        httpServer.start();
-
-        System.out.println("HTTP server is running on " + PORT + " port!");
+        HttpTaskServer server = new HttpTaskServer(Managers.getDefault());
+        server.start();
     }
 }
